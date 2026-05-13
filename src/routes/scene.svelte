d8c2a1cd588be50707cc1da0211aa0c2ff369721
Fox <urboifox@gmail.com>
2026-05-13 19:42:44 +0300

    ui: adjust scene lighting

--- src/routes/scene.svelte
+++ src/routes/scene.svelte
@@ -29,8 +29,9 @@
     }}
 />
 
-<T.DirectionalLight position={[8, 10, 5]} castShadow />
-<T.AmbientLight intensity={0.5} />
+<T.DirectionalLight intensity={0.5} position={[8, 10, 5]} castShadow color="#ff3e00" />
+<T.DirectionalLight intensity={0.5} position={[8, 10, 5]} castShadow />
+<T.AmbientLight intensity={0.3} />
 
 <T.Group position.y={meshY} position.z={1} {scale} rotation.y={-Math.PI / 4} position.x={meshX}>
     <GLTF url="/models/fox.glb" castShadow receiveShadow bind:gltf />
