926f8c12c5f76684bbd19ec082d8e0536dc46df8
Fox <urboifox@gmail.com>
2026-05-15 21:52:12 +0300

    dev: add scroll trigger markers

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,3 +2,5 @@ import { dev } from '$app/environment';
 import { injectAnalytics } from '@vercel/analytics/sveltekit';
 
 injectAnalytics({ mode: dev ? 'development' : 'production' });
+
+export const prerender = true;
